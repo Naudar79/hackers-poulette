1c729d89d1b2f169fb849544c92c8d88917bec03
Naudar79 <naudar79@gmail.com>
2021-03-24 10:56:00 +0100

    Update cible.php

--- cible.php
+++ cible.php
@@ -13,5 +13,23 @@
     <p>Country : <?php echo $_POST['country'] ?></p>
     <p>Reason : <?php echo $_POST['why'] ?></p>
     <p>Message : <?php echo $_POST['message'] ?></p>
+    <?php
+        $msg = "Nom:\t$fName\n";
+        $msg = "Nom:\t$lName\n";
+        $msg = "Nom:\t$genre\n";
+        $msg .= "E-Mail:\t$eMail\n";
+        $msg = "Nom:\t$country\n";
+        $msg = "Nom:\t$why\n";
+        $msg .= "Message:\t$message\n\n";
+        //Pourait continuer ainsi jusqu'à la fin du formulaire
+
+        $recipient = "tonCourriel@mail.com";
+        $subject = "Formulaire";
+
+        $mailheaders = "From: Mon test de formulaire<> \n";
+        $mailheaders .= "Reply-To: $email\n\n";
+
+        mail($recipient, $subject, $msg, $mailheaders);
+    ?>
 </body>
 </html>
